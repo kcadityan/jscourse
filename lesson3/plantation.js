433879c0c621c514e0a53908f1172c85676b4fe4
kcadityan <kcadityan@gmail.com>
2022-11-01 19:26:25 +0530

    nested_for_loop_example

---
+++ lesson3/plantation.js
@@ -0,0 +1,13 @@
+for (let fig_coco_counter = 0; fig_coco_counter < 5; fig_coco_counter++) {
+
+    process.stdout.write('🌳')
+    for (let coco_counter = 0; coco_counter < 3; coco_counter++) {
+        process.stdout.write('🌴')
+    }
+
+}
+console.log('')
+//output should be this ==> 🌳🌴🌴🌴🌳🌴🌴🌴🌳🌴🌴🌴
+
+
+
