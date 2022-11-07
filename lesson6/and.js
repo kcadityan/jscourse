b11a548bec71c98c2067eff1fef84de3804f2e48
kcadityan <kcadityan@gmail.com>
2022-11-07 19:02:41 +0530

    demo of how to use or

---
+++ lesson6/and.js
@@ -0,0 +1,10 @@
+// 'and' is written like this : &&
+
+let age = 4
+let gender= 'girl'
+if (age < 5 && gender==='boy' ) {
+    console.log('play with a car')
+}
+if (age < 5 && gender==='girl' ) {
+    console.log('play with a doll')
+}
