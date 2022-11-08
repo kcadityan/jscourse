9b0bc05a3bc6a53ef2825377e712ae5acc8a5564
kcadityan <kcadityan@gmail.com>
2022-11-08 11:38:58 +0530

    hw of lesson 6 and 7

---
+++ lesson8_hw/and_or.js
@@ -0,0 +1,10 @@
+let age = 5
+if (age < 5 || age === 5) {
+    console.log('stay at home and play ')
+}
+else if (age > 5 && age < 10) {
+    console.log('go to school')
+}
+else {
+    console.log('go to work')
+}
